e5c01a992ef88fcaf2c085dd772bf7462dee7f0a
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-11-16 21:14:47 +0000

    Added tests for getting Period objects from recurring period plugins.

--- tests/src/Kernel/RollingIntervalTest.php
+++ tests/src/Kernel/RollingIntervalTest.php
@@ -12,6 +12,83 @@ use Drupal\recurring_period\Plugin\RecurringPeriod\RecurringPeriodInterface;
 class RollingIntervalTest extends RecurringPeriodTestBase {
 
   /**
+   * Tests the TODO.
+   *
+   * @dataProvider rollingIntervalProvider
+   */
+  public function testRollingInterval($configuration, $start_date, $period_dates) {
+    $rolling_interval_plugin = $this->recurringPeriodManager->createInstance('rolling_interval', $configuration);
+
+    $timezone_london = new \DateTimeZone('Europe/London');
+
+    $start_date_time = new \DateTimeImmutable($start_date, $timezone_london);
+
+    $period = $rolling_interval_plugin->getPeriod($start_date_time);
+
+    $first_period_dates = array_shift($period_dates);
+    $this->assertEquals($first_period_dates[0], $period->getStartDate()->format(\DateTimeInterface::ATOM));
+    $this->assertEquals($first_period_dates[1], $period->getEndDate()->format(\DateTimeInterface::ATOM));
+
+    while ($next_period_dates = array_shift($period_dates)) {
+      // TODO: also check the value of calculateDate().
+
+      $period = $rolling_interval_plugin->getNextPeriod($period);
+
+      $this->assertEquals($next_period_dates[0], $period->getStartDate()->format(\DateTimeInterface::ATOM));
+      $this->assertEquals($next_period_dates[1], $period->getEndDate()->format(\DateTimeInterface::ATOM));
+    }
+  }
+
+  public function rollingIntervalProvider() {
+    return [
+      '1 month' => [
+        // Configuration.
+        [
+          'interval' => [
+            'period' => 'month',
+            'interval' => 1,
+          ],
+        ],
+        // Start date.
+        '2017-01-01T09:00:00',
+        // Successive period dates.
+        [
+          ['2017-01-01T09:00:00+00:00', '2017-02-01T09:00:00+00:00'],
+          ['2017-02-01T09:00:00+00:00', '2017-03-01T09:00:00+00:00'],
+          // The end of this period is after the change to DST, so the offset
+          // changes from 0 to +1.
+          ['2017-03-01T09:00:00+00:00', '2017-04-01T09:00:00+01:00'],
+          ['2017-04-01T09:00:00+01:00', '2017-05-01T09:00:00+01:00'],
+          ['2017-05-01T09:00:00+01:00', '2017-06-01T09:00:00+01:00'],
+          ['2017-06-01T09:00:00+01:00', '2017-07-01T09:00:00+01:00'],
+          ['2017-07-01T09:00:00+01:00', '2017-08-01T09:00:00+01:00'],
+          ['2017-08-01T09:00:00+01:00', '2017-09-01T09:00:00+01:00'],
+          ['2017-09-01T09:00:00+01:00', '2017-10-01T09:00:00+01:00'],
+          ['2017-10-01T09:00:00+01:00', '2017-11-01T09:00:00+00:00'],
+          ['2017-11-01T09:00:00+00:00', '2017-12-01T09:00:00+00:00'],
+          ['2017-12-01T09:00:00+00:00', '2018-01-01T09:00:00+00:00'],
+        ],
+      ],
+      '2 weeks' => [
+        // Configuration.
+        [
+          'interval' => [
+            'period' => 'week',
+            'interval' => 2,
+          ],
+        ],
+        // Start date.
+        '2017-01-01T09:00:00',
+        // Successive period dates.
+        [
+          ['2017-01-01T09:00:00+00:00', '2017-01-15T09:00:00+00:00'],
+          ['2017-01-15T09:00:00+00:00', '2017-01-29T09:00:00+00:00'],
+        ],
+      ],
+    ];
+  }
+
+  /**
    * Tests a 2 week interval in UTC.
    */
   public function test2WeekInterval() {
